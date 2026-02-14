bb598ca791f6cb9ed5967e76eb6157343ee3bf2d
Prathamesh910 <prathameshgiradkar910@gmail.com>
2026-02-14 11:27:37 +0530

    Created Player component which will be visible in the bottom of the ui...

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
 
 const App = () => {
-
   return (
-      <div className="h-screen bg-black">
-      
+    <div className="h-screen bg-black">
       <div className="h-[90%] flex">
-<Sidebar/>
+        <Sidebar />
       </div>
+      <Player />
     </div>
   );
-}
-
+};
 
 export default App;
