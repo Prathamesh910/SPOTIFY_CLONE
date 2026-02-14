e8b96f6042c17f855251a66004eada29037f16fe
Prathamesh910 <prathameshgiradkar910@gmail.com>
2026-02-14 16:12:24 +0530

    Completed displayhome ui component...

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -27,7 +27,7 @@ const Player = () => {
 
         <div className="flex items-center gap-5">
           <p>2.04</p>
-          <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
+          <div className="w-[60vw] max-w-125 bg-gray-300 rounded-full cursor-pointer">
             <hr className="h-1 border-none w-0 bg-green-800 rounded-full" />
           </div>
           <p>3.20</p>
